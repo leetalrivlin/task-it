a8c70dc86fac4e1439dc8a90a3635c531f231582
lizamir <lizamir2@gmail.com>
2021-03-17 12:30:22 +0200

    run

--- frontend/src/views/board-details.vue
+++ frontend/src/views/board-details.vue
@@ -1,13 +1,16 @@
 <template>
-  
+  <section class="board-details">
+      <h1>hii</h1>
+    <board-header />
+    <section class="group-container"></section>
+    <router-view />
+  </section>
 </template>
 
 <script>
+import boardHeader from "../cmps/board-header.vue";
 export default {
-
-}
+  name: "boardDetails",
+  components: { boardHeader },
+};
 </script>
-
-<style>
-
-</style>
