6986fd2f7d1a2c4b7d52d5eeecf3f95f6e190c42
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-03-19 11:54:06 +0200

    attachment

--- frontend/src/cmps/task/task-details/task-attachment.vue
+++ frontend/src/cmps/task/task-details/task-attachment.vue
@@ -3,12 +3,20 @@
       <i class="el-icon-paperclip d-icon task-details-icon"></i>
       <div class="d-content">
           <h1 class="task-details-header">Attachment</h1>
-          <section class="flex">
-              <img src="../../../assets/imgs/task-attachment-2.jpg" alt="">
+          <li class="task-attachment flex">
+              <div class="img-container"><img src="../../../assets/imgs/task-attachment-2.jpg" alt=""></div>
               <div class="img-desc">
-                  <h1>Image title</h1>
+                  <h1>Image.jpg</h1>
+                  <p>Added (createdAt) - <a href="">Comment</a> - <a href="">Delete</a> - <a href="">Edit</a></p>
               </div>
-          </section>
+          </li>
+          <li class="task-attachment flex">
+              <div class="img-container"><img src="../../../assets/imgs/task-attachment-1.jpg" alt=""></div>
+              <div class="img-desc">
+                  <h1>Image.jpg</h1>
+                  <p>Added (createdAt) - <a href="">Comment</a> - <a href="">Delete</a> - <a href="">Edit</a></p>
+              </div>
+          </li>
       </div>
       
   </section>
