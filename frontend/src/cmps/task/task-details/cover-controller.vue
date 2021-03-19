f8b8adc1b4c58b54b5662092d15d8928b71ced18
chen-sella <chensella1@gmail.com>
2021-03-19 16:08:24 +0200

    task-cover&cntrl

---
+++ frontend/src/cmps/task/task-details/cover-controller.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="cover-controller">
+    <header class="flex align-center justify-center">
+      <p>Cover</p>
+      <i class="el-icon-close close-cntrl" @click="closeController"></i>
+    </header>
+    <hr class="board-menu-header-divider" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'coverControllers',
+  methods: {
+    closeController() {
+      this.$emit('closeCntrl');
+    },
+  },
+};
+</script>
+
