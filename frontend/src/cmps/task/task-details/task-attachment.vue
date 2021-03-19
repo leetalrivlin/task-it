b2149298b0b0929ca9473450fd1ad2cf69e81a91
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-03-19 11:15:55 +0200

    attachment

--- frontend/src/cmps/task/task-details/task-attachment.vue
+++ frontend/src/cmps/task/task-details/task-attachment.vue
@@ -1,6 +1,16 @@
 <template>
-  <section>
-      <h1>task attachment</h1>
+  <section class="task-attachment-container d-desc">
+      <i class="el-icon-paperclip d-icon task-details-icon"></i>
+      <div class="d-content">
+          <h1 class="task-details-header">Attachment</h1>
+          <section class="flex">
+              <img src="../../../assets/imgs/task-attachment-2.jpg" alt="">
+              <div class="img-desc">
+                  <h1>Image title</h1>
+              </div>
+          </section>
+      </div>
+      
   </section>
 </template>
 
