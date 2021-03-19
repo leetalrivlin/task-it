ea9c201b87a9ae37784bd6a1d4188c47e2d0f31b
chen-sella <chensella1@gmail.com>
2021-03-19 18:29:39 +0200

    menu fixed

---
+++ frontend/src/cmps/board/board-menu/menu-main.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="menu-main">
+    <a class="menu-main-item flex align-center" @click="toggleAbout">
+      <i class="el-icon-info icon"></i>
+      <span>About this board </span>
+    </a>
+    <a class="menu-main-item flex align-center" @click="toggleBgc">
+      <font-awesome-icon icon="square" class="icon" />
+      <span>Change background </span>
+    </a>
+    <a class="menu-main-item flex align-center" @click="toggleSearch">
+      <i class="el-icon-search icon"></i>
+      <span>Search cards </span>
+    </a>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'menu-main',
+  methods: {
+    toggleAbout() {
+      this.$emit('openAbout');
+    },
+    toggleBgc() {
+      this.$emit('openBgc');
+    },
+    toggleSearch() {
+      this.$emit('openSearch');
+    },
+  },
+};
+</script>
