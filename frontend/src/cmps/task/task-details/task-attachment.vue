d034259d02f2c3d0b7c4fb64a8566f48a333ad2b
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-03-19 12:08:51 +0200

    attachment

--- frontend/src/cmps/task/task-details/task-attachment.vue
+++ frontend/src/cmps/task/task-details/task-attachment.vue
@@ -3,20 +3,22 @@
       <i class="el-icon-paperclip d-icon task-details-icon"></i>
       <div class="d-content">
           <h1 class="task-details-header">Attachment</h1>
-          <li class="task-attachment flex">
-              <div class="img-container"><img src="../../../assets/imgs/task-attachment-2.jpg" alt=""></div>
+          <!-- Should be replaced to one li with a v-for on the images: -->
+          <li class="task-attachment flex"> 
+              <div class="img-container ratio-attachment"><img src="../../../assets/imgs/task-attachment-2.jpg" alt=""></div>
               <div class="img-desc">
                   <h1>Image.jpg</h1>
-                  <p>Added (createdAt) - <a href="">Comment</a> - <a href="">Delete</a> - <a href="">Edit</a></p>
+                  <p><a href="">Delete</a> - <a href="">Edit</a></p>
               </div>
           </li>
           <li class="task-attachment flex">
-              <div class="img-container"><img src="../../../assets/imgs/task-attachment-1.jpg" alt=""></div>
+              <div class="img-container ratio-attachment"><img src="../../../assets/imgs/task-attachment-1.jpg" alt=""></div>
               <div class="img-desc">
                   <h1>Image.jpg</h1>
-                  <p>Added (createdAt) - <a href="">Comment</a> - <a href="">Delete</a> - <a href="">Edit</a></p>
+                  <p><a href="">Delete</a> - <a href="">Edit</a></p>
               </div>
           </li>
+          <a class="el-btn-details emptyChecklist">Add an attachment</a>
       </div>
       
   </section>
