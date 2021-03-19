20c371b02725ce08377742680af2da37ea63aae9
lizamir <lizamir2@gmail.com>
2021-03-19 14:11:43 +0200

    menu

---
+++ frontend/src/cmps/board/about-board.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about-this-board-section">
+    <div class="header flex align-center">
+      <!-- <font-awesome-icon icon="user" class="icon" /> -->
+      <i class="el-icon-user icon"></i>
+       <h4>Made by</h4> 
+    </div>
+  </div>
+</template>
+
+<script>
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faUser } from '@fortawesome/pro-regular-svg-icons';
+// library.add(faUser);
+export default {};
+</script>
+
+<style>
+</style>
--- frontend/src/cmps/board/board-menu.vue
+++ frontend/src/cmps/board/board-menu.vue
@@ -1,15 +1,19 @@
 <template>
   <section class="board-menu">
-    <header class="menu-header flex align-center justify-center">
-      <h4>Menu</h4>
+    <header class="menu-header flex align-center justify-center ">
+      <i v-if="isBack" class="el-icon-arrow-left icon-back" @click="backMenu"></i>
+      <h4>{{ title }}</h4>
       <i class="el-icon-close close-menu" @click="closeMenue"></i>
     </header>
     <hr class="board-menu-header-divider" />
     <div class="menu-content flex column">
       <div class="board-settings-menu">
-        <a class="board-settings-menu-item flex align-center">
+        <a
+          class="board-settings-menu-item flex align-center"
+          @click="toggleAbout"
+        >
           <i class="el-icon-info icon"></i>
-          <span>About this board</span>
+          <span>About this board </span>
         </a>
         <a class="board-settings-menu-item flex align-center">
           <font-awesome-icon icon="square" class="icon" />
@@ -20,24 +24,56 @@
           <span>Search cards </span>
         </a>
       </div>
+      <about-board v-if="isAbout && isOption" />
     </div>
   </section>
 </template>
 
 <script>
 import { faSquare } from '@fortawesome/free-solid-svg-icons';
-import { library } from '@fortawesome/fontawesome-svg-core'
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import aboutBoard from './about-board';
 library.add(faSquare);
 export default {
   data() {
-    return {};
+    return {
+      isBack:false,
+      isAbout: false,
+      isBgc: false,
+      isSearch: false,
+      isOption: false,
+    };
   },
   methods: {
     closeMenue() {
       console.log('closing');
       this.$emit('closeMenu');
     },
+    toggleAbout() {
+      this.isAbout = true;
+      this.isBack = true;
+    },
+    backMenu(){
+       this.isAbout = false;
+      this.isOption =true;
+      this.isBack = false;
+      this.isBgc= false;
+      this.isSearch= false;
+    }
+  },
+  computed: {
+    title() {
+      return !this.isOption
+        ? 'Menu'
+        : this.isAbout
+        ? 'About this board'
+        : this.isBgc
+        ? 'Change background'
+        : 'Search cards';
+    },
+  },
+  components: {
+    aboutBoard,
   },
 };
 </script>
