12c7c251ce9bc96d7da9530d6b7205526fb889c3
chen-sella <chensella1@gmail.com>
2021-03-21 11:02:47 +0200

    main scroller

--- frontend/src/cmps/board/board-header.vue
+++ frontend/src/cmps/board/board-header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="flex align-center space-between header-layout board-header">
       <el-button class="el-btn board-title">{{ board.title }}</el-button>
-      <el-button class="el-btn board-menu-btn" icon="el-icon-more" @click="isMenuOpen = true">Show Menu</el-button>
+      <el-button class="el-btn board-menu-btn" icon="el-icon-more icon" @click="isMenuOpen = true">Show Menu</el-button>
     <transition name="slide">
       <board-menu v-if="isMenuOpen" @closeMenu="isMenuOpen = false" />
     </transition>
