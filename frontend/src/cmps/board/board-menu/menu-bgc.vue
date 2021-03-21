32f39a17e14b4b1e4333cc64627058eedcaeac9c
lizamir <lizamir2@gmail.com>
2021-03-21 02:12:22 +0200

    menu-colors

--- frontend/src/cmps/board/board-menu/menu-bgc.vue
+++ frontend/src/cmps/board/board-menu/menu-bgc.vue
@@ -1,19 +1,58 @@
 <template>
-  <div class="about-this-board-section">
-    <div class="header flex align-center">
-      <!-- <font-awesome-icon icon="user" class="icon" /> -->
-      <i class="el-icon-user icon"></i>
-       <h4>Made by</h4> 
+  <div v-if="isBgc" class="flex justify-center">
+    <div class="select">
+      <div class="select-photo" @click="showPhotos">Photos</div>
+      <div class="select-color" @click="showPallete">Colors</div>
+    </div>
+  </div>
+  <div v-else>
+    <div v-if="isColors" class="div">
+      <cover-color-palettte @colorPicked="updateBoard" />
+       <!-- <ul class="clean-list">
+      <li
+        v-for="(color, idx) in colors"
+        :key="idx"
+        class="color-item"
+        :style="{
+          backgroundColor: color,
+        }"
+        @click="updateBoard(color)"
+      ></li>
+    </ul> -->
+    </div>
+    <div v-if="isPhotos" class="div">
+      <h2>photos</h2>
+      <!-- <cover-color-palettte @colorPicked="changeColor" /> -->
     </div>
   </div>
 </template>
 
 <script>
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { faUser } from '@fortawesome/pro-regular-svg-icons';
-// library.add(faUser);
-export default {};
+import coverColorPalettte from '../../task/task-details/cover-colorPalettte';
+export default {
+  data() {
+    return {
+      isBgc: true,
+      isColors: false,
+      isPhotos: false,
+    };
+  },
+  methods: {
+    showPallete() {
+      this.isColors = true;
+      this.isBgc = false;
+    },
+    showPhotos() {
+      this.isPhotos = true;
+      this.isBgc = false;
+    },
+    updateBoard(){
+      console.log('color');
+    }
+  },
+    components: {
+      coverColorPalettte,
+    },
+};
 </script>
 
-<style>
-</style>
