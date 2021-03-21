c5c0be9a5f85e860db26e70cf14a844f86da4a38
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-03-21 23:36:35 +0200

    delete todo menu

--- frontend/src/cmps/task/task-details/checklist-todo.vue
+++ frontend/src/cmps/task/task-details/checklist-todo.vue
@@ -1,5 +1,9 @@
 <template>
-  <li class="d-todos todo-item">
+  <li
+    class="d-todos todo-item"
+    @mouseover="todoOnFocus = true"
+    @mouseleave="todoOnFocus = false"
+  >
     <input
       type="checkbox"
       v-model="isDone"
@@ -13,21 +17,36 @@
     ></el-checkbox> -->
     <div class="d-todo-content flex align-center justify-center">
       <span>{{ todo.txt }}</span>
-      <i class="el-icon-more more-btn"></i>
+      <i v-if="todoOnFocus" class="el-icon-more more-btn" @click="openMenu">
+        <popup v-if="isOpenMenu" @closePopup="isOpenMenu = false">
+          <template v-slot:title>
+            <p>Item Actions</p>
+          </template>
+          <todo-popup @deleteTodo="deleteTodo" />
+        </popup>
+      </i>
     </div>
   </li>
 </template>
 
 <script>
+import popup from './popup.vue';
+import todoPopup from './todo-popup.vue';
 export default {
   props: {
     todo: {
       type: Object
     }
   },
+  components: {
+    todoPopup,
+    popup
+  },
   data() {
     return {
-      isDone: this.todo.isDone
+      isDone: this.todo.isDone,
+      isOpenMenu: false,
+      todoOnFocus: false
     };
   },
 
@@ -35,6 +54,10 @@ export default {
     toggleTodo() {
       this.todo.isDone = this.isDone;
       this.$emit('updateTodo', this.todo);
+    },
+    openMenu() {
+      this.isOpenMenu = !this.isOpenMenu;
+      this.todoOnFocus = (this.isOpenMenu) ? true : false;
     },
     deleteTodo() {
       this.$emit('deleteTodo', this.todo.id);
---
+++ frontend/src/cmps/task/task-details/todo-popup.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="todo-popup" @click.stop>
+    <p class="delete-btn" @click="deleteTodo">Delete</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'todoPopUp',
+  methods: {
+    deleteTodo() {
+      this.$emit('deleteTodo');
+    }
+  }
+};
+</script>
