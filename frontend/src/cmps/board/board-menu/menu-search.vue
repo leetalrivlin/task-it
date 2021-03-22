bc9e1ab2d332d223467adf98ec0d860ca1763d9b
lizamir <lizamir2@gmail.com>
2021-03-22 11:17:52 +0200

    filterby

--- frontend/src/cmps/board/board-menu/menu-search.vue
+++ frontend/src/cmps/board/board-menu/menu-search.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="search-section">
-    <el-input prefix-icon="el-icon-search" v-model="filterBy.txt"> </el-input>
-    <p class="desc-filter">Search by term, label, member, or due time.</p>
-
-    <hr />
-  </div>
+  <section class="search-section">
+    <div class="filter-text">
+      <el-input prefix-icon="el-icon-search" v-model="filterBy.txt"> </el-input>
+      <p class="desc-filter">Search by term, label, member, or due time.</p>
+      <hr />
+    </div>
+    <div class="filter-lables">
+      <ul class="clean-list flex column">
+        <li v-for="label in labels" :key="label.id" class="list-item">
+          <span :style="{ backgroundColor: label.color }">{{
+            label.title
+          }}</span>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -13,8 +23,16 @@ export default {
     return {
       filterBy: {
         txt: '',
+        labels: [],
+        members: [],
       },
     };
+  },
+  computed: {
+    labels() {
+      console.log(this.$store.getters.boardLabels);
+      return this.$store.getters.boardLabels;
+    },
   },
 };
 </script>
