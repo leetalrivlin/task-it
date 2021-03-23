59d124d47ec33ac627ecc167b971d95d3ec19730
chen-sella <chensella1@gmail.com>
2021-03-23 20:14:15 +0200

    started homepage

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,27 +1,22 @@
 <template>
-  <section class="main-content main-layout">
-    <h1>Home Page</h1>
-    <h5>
-      Powered up Frontend 
-    </h5>
+  <section class="main-content main-layout home-container">
+    <div class="flex column align-center hero">
+      <h1>Task-it helps teams move work forward.</h1>
+      <router-link :to="`/board/6059cc912a005fb1854a5d50`"
+        >Try it now</router-link
+      >
+    </div>
   </section>
-
-  
 </template>
 
 <script>
 export default {
   name: 'home',
   data() {
-    return {
-    }
+    return {};
   },
-  computed: {
-  },
-  created() {
-  },
-  methods: {
-  }
-  
-}
+  computed: {},
+  created() {},
+  methods: {},
+};
 </script>
