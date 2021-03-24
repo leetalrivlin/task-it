45c1bacdf78f302d2ab43f271d9fcc83ae0bacc8
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-03-24 14:27:11 +0200

    move task popup

--- frontend/src/cmps/task/task-details/details-popup/move-task-popup.vue
+++ frontend/src/cmps/task/task-details/details-popup/move-task-popup.vue
@@ -1,18 +1,31 @@
 <template>
   <section class="move-task-container">
     <p>Select Destination</p>
-    <div>
-      <p>List</p>
-      <el-select v-model="value" placeholder="Select">
+    <section class="flex space-between">
+      <div>
+        <p>List</p>
+        <el-select v-model="chosenGroup" @change="showGroupTasks">
+          <el-option
+            v-for="group in groups"
+            :key="group.id"
+            :value="group.id"
+            :label="group.title"
+          >
+          </el-option>
+        </el-select>
+      </div>
+      <div>
+        <p>Position</p>
+        <!-- <el-select v-model="value">
         <el-option
           v-for="group in groups"
           :key="group.id"
-          :label="group.title"
           :value="group.title"
         >
         </el-option>
-      </el-select>
-    </div>
+      </el-select> -->
+      </div>
+    </section>
   </section>
 </template>
 
@@ -27,17 +40,31 @@ export default {
     },
     groupId: {
       type: String
-    },
+    }
   },
   data() {
     return {
-      value: '',
+      chosenGroup: ''
     };
   },
   computed: {
-      currGroup() {
-          return this.groups.find(({id}) => id === this.groupId);
-      }
+    currGroup() {
+      return this.groups.find(({ id }) => id === this.groupId);
+    },
+    tasks() {
+      return this.currGroup;
+    },
+    tasksLength() {
+      var chosenGroup;
+      if (this.currGroup) chosenGroup = this.currGroup;
+      chosenGroup = this.groups.find(({ id }) => id === this.chosenGroup.id);
+      return chosenGroup.tasks.length;
+    }
+  },
+  methods: {
+    showGroupTasks() {
+      console.log('this.chosenGroup', this.chosenGroup);
+    }
   }
 };
 </script>
