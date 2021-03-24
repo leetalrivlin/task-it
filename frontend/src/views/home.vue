6a809c6f7bc5bde15fb97762fbb5937591a14b94
chen-sella <chensella1@gmail.com>
2021-03-24 12:18:49 +0200

    home page css edits

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,13 +1,15 @@
 <template>
-  <section class="main-content main-layout home-container">
-    <div class="hero flex align-center">
+  <section
+    class="flex column align-center main-content main-layout home-container"
+  >
+    <div class="hero flex space-between align-center">
       <div class="flex column hero-content">
         <h1>Task-it helps teams move work forward.</h1>
         <router-link :to="`/board/6059cc912a005fb1854a5d50`"
           >Try it now</router-link
         >
       </div>
-      <img src="../assets/vectors/home-page-img.png"> 
+      <img src="../assets/vectors/home-page-img.png" />
     </div>
   </section>
 </template>
