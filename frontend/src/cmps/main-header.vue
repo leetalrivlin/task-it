c205d8f8706e3e7571326dddacea6d68d15ec723
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-03-25 21:41:52 +0200

    bouncing logo in home

--- frontend/src/cmps/main-header.vue
+++ frontend/src/cmps/main-header.vue
@@ -8,15 +8,8 @@
         <router-link class="el-btn" to="/board">Boards</router-link>
       </div>
       <span class="flex justify-center align-center logo">
-        <!-- <router-link to="/"><div class="flex justify-center align-center"><span class="task-it-logo">Taskit</span><img src="~@/assets/imgs/task-it-logo-clr1-bounce.gif" class="gif-icon"></div></router-link></span -->
-        <router-link to="/"
-          ><div class="flex justify-center align-center">
-            <span class="task-it-logo">Taskit</span
-            ><img
-              src="~@/assets/imgs/task-it-icon-clr1.png"
-              class="gif-icon"
-            /></div></router-link
-      ></span>
+        <router-link to="/"><div class="flex justify-center align-center"><span class="task-it-logo">Taskit</span><img src="~@/assets/imgs/task-it-logo-clr1-bounce.gif" class="gif-icon"></div></router-link>
+        </span>
       <section
         class="window-overlay"
         @click="closeNav"
