fc2977c5e1f259e967f02abe44174c74a318ad93
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-03-26 18:25:09 +0300

    activity icon

--- frontend/src/cmps/board/board-menu/menu-activity.vue
+++ frontend/src/cmps/board/board-menu/menu-activity.vue
@@ -1,16 +1,16 @@
 <template>
   <section>
       <div class="header flex align-center">
-      <!-- <font-awesome-icon
-          class="d-icon fa-nav-icon icon-user"
-          :icon="['far', 'user']"
-        /> -->
-      <h4>Made by</h4>
+      <font-awesome-icon class="icon" icon="list-ul" />
+      <h4>Activity</h4>
     </div>
   </section>
 </template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faListUl } from '@fortawesome/free-solid-svg-icons';
+library.add(faListUl);
 export default {
 
 };
