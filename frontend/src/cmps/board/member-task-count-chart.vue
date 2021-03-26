eba68bd49067b2d7775a81c52cac7ab9191cda93
lizamir <lizamir2@gmail.com>
2021-03-26 11:18:57 +0300

    filter

--- frontend/src/cmps/board/member-task-count-chart.vue
+++ frontend/src/cmps/board/member-task-count-chart.vue
@@ -1,9 +1,6 @@
 <script>
 import { Bar } from 'vue-chartjs';
 
-Chart.defaults.global.defaultFontColor = '#faf3e0';
-Chart.defaults.global.elements.point.radius = 3;
-
 export default {
   extends: Bar,
   props: {
@@ -18,9 +15,6 @@ export default {
     },
   },
   mounted() {
-    // console.log(this.chartData.tasksCount);
-    // console.log(this.chartData.fullname);
-
     this.renderChart(
       {
         labels: this.chartData.fullname,
--- frontend/src/cmps/board/member-task-count.vue
+++ frontend/src/cmps/board/member-task-count.vue
@@ -13,6 +13,8 @@
 
 <script>
 import memberTaskCountChart from './member-task-count-chart.vue';
+
+Chart.defaults.global.defaultFontColor= 'white'
 export default {
   props: {
     board: {
@@ -27,7 +29,6 @@ export default {
         borderWidth: 1,
         hoverBorderColor: '#777',
         hoverBorderWidth: 3,
-
       },
       options: {
         title: {
@@ -35,43 +36,52 @@ export default {
           text: 'Tasks per member',
           fontSize: 25,
         },
+          scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+        },
         legend: {
           display: false,
         },
       },
     };
   },
-  created() {
-    var tasksToShow = [];
-    var membersPerTask = {};
-    this.board.groups.forEach((group) => {
-      group.tasks.forEach((task) => {
-        if (!task.members || !task.members.length) return;
-        tasksToShow.push(task);
+
+  methods: {
+    calcMemberPerTask() {
+      var tasksToShow = [];
+      var membersPerTask = {};
+      this.board.groups.forEach((group) => {
+        group.tasks.forEach((task) => {
+          if (!task.members || !task.members.length) return;
+          tasksToShow.push(task);
+        });
       });
-    });
-    console.log(tasksToShow);
-    tasksToShow.forEach((task) => {
-      task.members.forEach((member) => {
-        if (!membersPerTask.hasOwnProperty(member._id)) {
-          membersPerTask[member._id] = {
-            fullname: member.fullname,
-            tasksCount: 1,
-          };
-        } else {
-          membersPerTask[member._id].tasksCount++;
-        }
+      console.log(tasksToShow);
+      tasksToShow.forEach((task) => {
+        task.members.forEach((member) => {
+          if (!membersPerTask.hasOwnProperty(member._id)) {
+            membersPerTask[member._id] = {
+              fullname: member.fullname,
+              tasksCount: 1,
+            };
+          } else {
+            membersPerTask[member._id].tasksCount++;
+          }
+        });
       });
-    });
 
-    for (const member in membersPerTask) {
-      this.chartData.fullname.push(membersPerTask[member].fullname);
-      this.chartData.tasksCount.push(membersPerTask[member].tasksCount);
-    }
-
-    console.log(this.chartData.fullname);
-
-    var colors = [
+      for (const member in membersPerTask) {
+        this.chartData.fullname.push(membersPerTask[member].fullname);
+        this.chartData.tasksCount.push(membersPerTask[member].tasksCount);
+      }
+    },
+    colors(){
+        var colors = [
       'rgb(170, 79, 77)',
       '#4d96c9',
       '#D8DA7C',
@@ -85,6 +95,16 @@ export default {
 
     this.chartData.colors = colors;
     this.showChart = true;
+
+    }
+    
+    
+  },
+  created() {
+    this.calcMemberPerTask();
+    this.colors();
+
+    
   },
   components: {
     memberTaskCountChart,
