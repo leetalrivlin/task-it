a588d77009e3e0d66bac7e037d05f986bdcab856
chen-sella <chensella1@gmail.com>
2021-03-28 22:39:36 +0300

    home page css

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -2,14 +2,29 @@
   <section
     class="flex column align-center main-content main-layout home-container"
   >
-    <div class=" flex space-between align-center hero">
+    <div class="flex space-between align-center hero">
       <div class="flex column hero-content">
-        <h1 class="flex">Task-it helps teams move work forward.</h1>
-        <router-link :to="`/board/6059cc912a005fb1854a5d50`"
+        <h1 class="flex">A better way to manage tasks.</h1>
+        <p>
+          If you have time, <span>Task-it</span> helps you spend your time well.
+        </p>
+        <router-link :to="`/board/60606de5b8b4f44277b7f5da`"
           >Try it now</router-link
         >
       </div>
       <img src="../assets/vectors/home-page-img.png" />
+    </div>
+    <div class="main flex align-center">
+      <img src="../assets/vectors/shutterstock_1390480307.jpg">
+      <div class="main-content">
+        <ul>
+          <li>Invite your team members on click</li>
+          <li>Easy adding and deleting tasks</li>
+          <li>Simple and clear navigation</li>
+          <li>Eye pleasing interface</li>
+          <li>Get real-time updates with innovative Dashboards</li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
