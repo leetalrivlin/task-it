19be1b3650d047e10756cb891643ac0af9b64382
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-03-28 12:31:22 +0300

    changes

--- frontend/src/cmps/task/task-details.vue
+++ frontend/src/cmps/task/task-details.vue
@@ -230,7 +230,9 @@ export default {
       this.boardLabels.splice(labelIdx, 1, updatedLabel);
       const copyBoard = this.$clone(this.$store.getters.board);
       copyBoard.labels = this.boardLabels;
-      this.$store.dispatch({ type: 'updateBoard', board: copyBoard });
+      const txt = `updated board labels`;
+      const activity = this.setActivity(txt);
+      this.$store.dispatch({ type: 'updateBoard', payload:{ board: copyBoard, activity } });
     },
     setDueDate(dueDate) {
       this.task.dueDate = dueDate;
@@ -254,18 +256,20 @@ export default {
         : { fullname: 'Guest', imgUrl: '' };
       this.$store.dispatch({ type: 'updateTask', payload: { task, activity } });
     },
-    updateBoard(updatedGroup) {
+    updateBoard(updatedGroup, activity = {}) {
       const cloneBoard = this.$clone(this.$store.getters.board);
       const idx = cloneBoard.groups.findIndex(
         ({ id }) => id === updatedGroup.id
       );
       cloneBoard.groups.splice(idx, 1, updatedGroup);
-      this.$store.dispatch({ type: 'updateBoard', board: cloneBoard });
+      this.$store.dispatch({ type: 'updateBoard', payload:{ board: cloneBoard, activity }});
     },
     removeTask(taskId) {
       const taskIdx = this.group.tasks.findIndex(({ id }) => id === taskId);
       this.group.tasks.splice(taskIdx, 1);
-      this.updateBoard(this.group);
+      const txt = `removed ${this.task.title} from ${this.group.title}`;
+      const activity = this.setActivity(txt);
+      this.updateBoard(this.group, activity);
       this.closeDetails();
     }
   },
